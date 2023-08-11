b29ed729e8af71f4fe3c45b07b8363ad4c7b2522
Tarik <tariksalhiars@gmail.com>
2023-08-11 09:40:08 +0100

    ..

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
                 return response()->json(['error' => 'Invalid credentials'], 401);
             }
         } catch (JWTException $e) {
-            return response()->json(['error' => 'Could not create token'], 500);
+            return response()->json(['error' => 'Could not create token','err'=>$e->getMessage()], 500);
         }
 
         return response()->json(compact('token'));
